50074e4bc9446160c9d922ec58f158bf4e309405
Naveen Gidha <naveen.gidha@gmail.com>
2020-11-12 19:32:48 +0000

    Added sector filter; formatted numbers

--- analysis.py
+++ analysis.py
@@ -1,5 +1,19 @@
 import yfinance as yf, pandas as pd, shutil, os, time, glob
 from get_all_tickers import get_tickers as gt
+from enum import Enum
+
+class Sectors(Enum):
+    NON_DURABLE_GOODS = 'Consumer Non-Durables'
+    CAPITAL_GOODS = 'Capital Goods'
+    HEALTH_CARE = 'Health Care'
+    ENERGY = 'Energy'
+    TECH = 'Technology'
+    BASICS = 'Basic Industries'
+    FINANCE = 'Finance'
+    SERVICES = 'Consumer Services'
+    UTILITIES = 'Public Utilities'
+    DURABLE_GOODS = 'Consumer Durables'
+    TRANSPORT = 'Transportation'
 
 
 def get_OBV(stock, data):
@@ -43,81 +57,117 @@ def get_SR(stock, data):
         close = data.iloc[0,4]
         pivot = (high + low + close) / 3
         
-        r1 = (2 * pivot) - low
-        s1 = (2 * pivot) - high
+        r = (2 * pivot) - low
+        s = (2 * pivot) - high
         
-        r2 = pivot + (high - low)
-        s2 = pivot - (high - low)
-
-        r3 = r1 + (high - low)
-        s3 = s1 - (high - low)
-
-        return [r1, s1, r2, s2, r3, s3]
+        return [r, s]
 
     except Exception as e:
         print("Error calculating S/R levels for", stock, ":", e)
-        return 6 * [-1]
+        return 2 * [-1]
 
 
-# MAIN
-# API limits: 2,000/hour, 48,000/day
-
-api_calls = 0
-failures = 0
-not_imported = 0
-i=0
-results = []
-#tickers = gt.get_tickers_filtered(mktcap_min=75000, mktcap_max=10000000)
-tickers = ["NIO", "SQ"]
-
-while(i < len(tickers)) and (api_calls < 1800):
+def get_open(stock, previous_close, current_open):
+    """
+    Calculate opening %
+    """
     try:
-        # get stock data and save to individual CSVs
-        stock = tickers[i]
-        ticker_object = yf.Ticker(str(stock))
+        return (((current_open - previous_close) / previous_close) * 100)
+    except Exception as e:
+        print("Error calculating open % for", stock, ":", e)
+        return -1
 
-        # save historical data
-        historical_data = ticker_object.history(period="max")
-        historical_data.to_csv(stock+".csv")
 
-        # get stock historical data
-        stock_csv = glob.glob(stock+".csv")
-        data = pd.read_csv(stock_csv[0])
+def get_stock_details():
+    """
+    Main loop
+    API limits: 2,000/hour; 48,000/day
+    """
 
-        time.sleep(2)
+    # variables to keep track of success/failure calls to API
+    api_calls = 0
+    failures = 0
+    not_imported = 0
+
+    for sector in Sectors:
+        results = []
+        i = 0
+        tickers = gt.get_biggest_n_tickers(20, sectors = sector.value)
+        while(i < len(tickers)) and (api_calls < 1800):
+            try:
+                # get stock data and save to individual CSVs
+                stock = tickers[i]
+                ticker_obj = yf.Ticker(str(stock))
+                ticker_data = ticker_obj.history(period="3mo")
+                ticker_data.to_csv(stock+".csv")
+
+                # get stock historical data
+                stock_csv = glob.glob(stock+".csv")
+                data = pd.read_csv(stock_csv[0])
+
+                time.sleep(2)
         
-        api_calls += 1
-        failures = 0
-        i += 1
+                api_calls += 1
+                failures = 0
+                i += 1
 
-        # stock OBV for last 10 trading days
-        stock_obv = get_OBV(stock, data.tail(10))
-        # stock S/R levels for 1 trading day
-        # for multiple S/R levels, run data through for loop for N rows in data (include date)
-        stock_SR = get_SR(stock, data.tail(1))
+                stock_SR = get_SR(stock, 
+                                  data.tail(2))
 
-        results.append([stock, data.tail(2).iloc[0,4], data.tail(1).iloc[0,1], stock_SR[0], stock_SR[1], stock_SR[2], stock_SR[3], data.tail(1).iloc[0,4], stock_obv])
+                open_pcntg = get_open(stock, 
+                                      data.tail(2).iloc[0,4], 
+                                      data.tail(1).iloc[0,1])
 
-        df = pd.DataFrame(results, columns = ["Stock", "Previous Close", "Open", "R1", "S1", "R2", "S2", "Close/Current", "OBV"])
-        # rank by OBV
-        df["Ranked"] = df["OBV"].rank(ascending = False)
-        # sort ranked stocks
-        df.sort_values("OBV", inplace = True, ascending = False)
-        # save to CSV
-        df.to_csv("DAILY_REPORT.csv", index = False)
+                if not ticker_obj.info['trailingEps']:
+                    trailingEPS = -1
+                else:
+                    trailingEPS = round(float(ticker_obj.info['trailingEps']), 2)
 
-    except ValueError:
-        print("Yahoo Finance Backend Error")
-        if failures > 5:  # Move on to the next ticker if the current ticker fails more than 5 times
-            i+=1
-            not_imported += 1
-        api_calls += 1
-        failures += 1
+                if not ticker_obj.info['forwardEps']:
+                    forwardEPS = -1
+                else:
+                    forwardEPS = round(float(ticker_obj.info['forwardEps']), 2)
 
-print("Processed: " + str(i - not_imported) + "/" + str(len(tickers)))
+                trailingPE = round(data.tail(1).iloc[0,4] / trailingEPS, 2)
 
-top = df.head(10)
-print(top.to_string(index = False))
+                if not ticker_obj.info['forwardPE']:
+                    forwardPE = -1
+                else:
+                    forwardPE = round(float(ticker_obj.info['forwardPE']), 2)
 
-bottom = df.tail(10)
-print(bottom.to_string(index = False))
+                results.append([stock, 
+                                round(data.tail(2).iloc[0,4], 2),
+                                round(data.tail(1).iloc[0,1], 2), 
+                                round(open_pcntg, 2), 
+                                round(data.tail(1).iloc[0,4], 2), 
+                                round(stock_SR[0], 2), 
+                                round(stock_SR[1], 2), 
+                                f"{ticker_obj.info['marketCap']:,}", 
+                                f"{data.tail(1).iloc[0,5]:,}"])
+
+                df = pd.DataFrame(results, 
+                                  columns = ["Stock", 
+                                             "Previous close", 
+                                             "Open price",
+                                             "Open %", 
+                                             "Close/Current", 
+                                             "Resistance", 
+                                             "Support", 
+                                             "Market cap", 
+                                             "Volume"])
+            
+                df.sort_values("Market cap", inplace = True, ascending = False)
+                df.to_csv("reports/" + sector.name + ".csv", index = False)
+            except Exception as e:
+                print("Error:", e)
+                if failures > 5:  # Move on to the next ticker if the current ticker fails more than 5 times
+                    i+=1
+                    not_imported += 1
+                api_calls += 1
+                failures += 1
+
+        print(sector.name)
+        print(df.to_string(index = False))
+
+#main
+get_stock_details()
